f364dd44cb48c4ef6210ba8e1f2fab66d13b1367
LucasMtss <lucasrmts@gmail.com>
2023-08-17 17:00:42 -0300

    chore: fix repository

--- src/database/index.ts
+++ src/database/index.ts
@@ -3,9 +3,9 @@ import { Sequelize } from 'sequelize';
 const sequelize = new Sequelize({
   dialect: 'mysql',
   host: 'localhost',
-  username: '',
-  password: '',
-  database: '',
+  username: 'lucas',
+  password: '1234#Lrm',
+  database: 'trampos_db',
 });
 
 export { sequelize };
